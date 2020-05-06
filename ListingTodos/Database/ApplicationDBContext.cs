b8500769350c9ac06ea123e90276958b376d3dd0
MarekSchutz <schutz.marek@gmail.com>
2020-05-06 11:27:41 +0200

    update config

--- ListingTodos/Database/ApplicationDBContext.cs
+++ ListingTodos/Database/ApplicationDBContext.cs
@@ -12,6 +12,8 @@ namespace ListingTodos.Database
         public ApplicationDBContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=ec2-54-247-89-181.eu-west-1.compute.amazonaws.com;Database=da99lvpjk0ipdr;Username=kdxedzxohvmymc;Password=84a675e0c8a29b50270666ecc5d75b25dedbe36329404d790cdd9a45a24bfee8");
         public DbSet<Todo> Todos { get; set; }
         public DbSet<User> Users { get; set; }
     }
